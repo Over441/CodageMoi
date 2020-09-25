2769276d80ca83be352f5488994e9c31dca97a03
Over441 <48887024+Over441@users.noreply.github.com>
2020-09-25 22:08:52 +0200

    Easter egg
    -Ajout d'un easter egg.

---
+++ Site web dessart/Mes sites/Tristan et Joris/connectionPRINCIPAL.js
@@ -0,0 +1,18 @@
+function passWord() {
+    var testV = 1;
+    var pass1 = prompt('Veuillez entrer le mot de passe','');
+    while (testV < 3) {
+    if (!pass1)
+    history.go(-1);
+    if (pass1.toLowerCase() == "tk78OZL") {
+    window.open('https://www.youtube.com/watch?v=CrtYDE0s_AI');
+    break ;
+    }
+    testV+=1;
+    var pass1 =
+    prompt('Erreur','Mot de passe');
+    }
+    if (pass1.toLowerCase()!="sesame" & testV ==3)
+    history.go(-1);
+    return " ";
+    }
