4e15728aafc48f6fbf1cb8026df7d28a461cf7c0
Over441 <48887024+Over441@users.noreply.github.com>
2020-09-26 11:55:04 +0200

    Ajout du grand bo

---
+++ Site web dessart/Mes sites/Le grand bo/CGH.js
@@ -0,0 +1,5 @@
+document.querySelector('.bouton-cgh').addEventListener('click', function(e){
+    e.preventDefault();
+    var url="https://www.cgh-residences.com/hiver/residences/le-village-de-lessy-9";
+    window.open(url, "Reservation");
+});
---
+++ Site web dessart/Mes sites/Le grand bo/Roc des tours.js
@@ -0,0 +1,5 @@
+document.querySelector('.bouton-roc-des-tours').addEventListener('click', function(e){
+    e.preventDefault();
+    var url="https://www.mgm-hotels-residences.com/residence-le-roc-des-tours-le-grand-bornand-chinaillon";
+    window.open(url, "Reservation");
+});
