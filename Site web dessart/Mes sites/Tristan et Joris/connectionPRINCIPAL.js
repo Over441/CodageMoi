eb5fb8bb03ceb20f4b9af55a125391feeff33b73
Over441 <48887024+Over441@users.noreply.github.com>
2020-10-01 21:36:42 +0200

    ReReReReChangement

--- Site web dessart/Mes sites/Tristan et Joris/connectionPRINCIPAL.js
+++ Site web dessart/Mes sites/Tristan et Joris/connectionPRINCIPAL.js
@@ -4,7 +4,7 @@ function passWord() {
     while (testV < 3) {
     if (!pass1)
     history.go(-1);
-    if (pass1.toLowerCase() == "OZL") {
+    if (pass1.toLowerCase() == "ozl") {
     window.open('https://www.youtube.com/watch?v=CrtYDE0s_AI');
     break ;
     }
