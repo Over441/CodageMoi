c1a41857f1986fa1c0f6aad52f77a393153104ba
Over441 <dessart.loo@orange.fr>
2022-01-01 22:57:53 +0100

    Fur

--- CodageMoi/Mes sites/Tristan et Joris/viens_par_la.js
+++ CodageMoi/Mes sites/Tristan et Joris/viens_par_la.js
@@ -4,7 +4,7 @@ function Word() {
     while (testV < 3) {
     if (!pass1)
     history.go(-1);
-    if (pass1.toLowerCase() == "vapeisloading100%") {
+    if (pass1.toLowerCase() == "furryisloading100%") {
     window.open('https://over441.github.io/CodageMoi/CodageMoi/Mes%20sites/Tristan%20et%20Joris/biote.html');
     break ;
     }
